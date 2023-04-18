86bff2ffcbdd209e34758fb69c0ae74355fc5885
CharlieApell <>
2023-04-18 13:36:42 +0000

    added js to contactform and emailjs

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,15 @@
+function sendMail(contactForm) {
+    emailjs.send("service_2ky4bvg","template_dmh6c8g", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+}
